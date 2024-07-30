8431b8cdc1aa2bf99c2400f045c4a23a1226556a
Tim <timothycoelho@gmail.com>
2024-07-30 19:17:52 -0400

    RGB fix.

--- Fractal Image Generator/RGB.h
+++ Fractal Image Generator/RGB.h
@@ -5,12 +5,11 @@ namespace frctl
 {
 	struct RGB
 	{
-		double r;
-		double g;
-		double b;
+		double r = 0.0;
+		double g = 0.0;
+		double b = 0.0;
 
 		RGB(double r, double g, double b);
-
 	};
 
 	RGB operator-(const RGB& first, const RGB& second);
