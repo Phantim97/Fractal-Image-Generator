6d3e2d7517ec786fd64f4050194e391028755b5d
Tim <timothycoelho@gmail.com>
2024-07-30 18:57:32 -0400

    Revamp for Bitmap Structures

--- Fractal Image Generator/bitmapFileHeader.h
+++ Fractal Image Generator/bitmapFileHeader.h
@@ -5,13 +5,13 @@
 
 #pragma pack(push, 2) //makes c++ allign the struct on 2 byte boundaries rather than using extra padding to bound it
 
-namespace frctl//cave of programming
+namespace frctl
 {
 	struct BitmapFileHeader //the pack allows this to be a readable file
 	{
-		int32_t fileSize = 0; // this makes sure we can be portable and use a 32 byte type int
+		int32_t file_size = 0; // this makes sure we can be portable and use a 32 byte type int
 		int32_t reserved = 0;
-		int32_t dataOffset = 0; //how much into the file the data begins
+		int32_t data_offset = 0; //how much into the file the data begins
 		char header[2]{ 'B','M' };
 	};
 }
